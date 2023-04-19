2acbebd30764ee8981e07c7f521196dcb92fc23e
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-04-19 16:29:38 -0700

    add my code into app.js file for the simple calculator app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  useState,
+  useRef
+} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+function App() { 
+  const inputRef = useRef(null); 
+  const resultRef = useRef(null); 
+  const [result, setResult] = useState(0); 
+ 
+  function plus(e) { 
+    e.preventDefault(); 
+    setResult((result) => result + Number(inputRef.current.value)); 
+  }; 
+ 
+  function minus(e) { 
+  	// Add the code for the minus function, model solution commented out. 
+    // e.preventDefault(); 
+    // const inputVal = inputRef.current.value; 
+    // const newResult = result - Number(inputVal); 
+    // setResult(newResult); 
+    e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value));
+  };
+ 
+  function times(e) { 
+    // Add the code for the plus function
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value));
+  }; 
+ 
+  function divide(e) { 
+    // Add the code for the divide function
+    e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value));
+  };
+ 
+  function resetInput(e) { 
+    // Add the code for the resetInput function
+    // Below is the model solution provided, I trie this and it didn't work as I wanted before I came up with my solution.
+    // inputRef.current.value = 0;  
+    e.preventDefault();
+    inputRef.current.value = " ";
+  }; 
+ 
+  function resetResult(e) { 
+  	// Add the code for the resetResult function, the model answer is commented out.
+    // e.preventDefault();
+    // setResult(0); 
+    e.preventDefault();
+    setResult((result) => result = 0);
+  }; 
+ 
+  return ( 
+    <div className="App"> 
+      <div> 
+        <h1>Simplest Working Calculator</h1> 
+      </div> 
+      <form> 
+        <p ref={resultRef}> 
+          {result} 
+        </p> 
+        <input
+          pattern="[0-9]" 
+          ref={inputRef} 
+          type="number" 
+          placeholder="Type a number" 
+        /> 
+        <button onClick={plus}>Add</button>
+        <button onClick={minus}>Subtract</button> 
+        <button onClick={times}>Multiply</button> 
+        <button onClick={divide}>Divide</button> 
+        <button onClick={resetInput}>Reset Input</button> 
+        <button onClick={resetResult}>Reset Result</button>
+      </form> 
+    </div> 
+  ); 
+} 
+ 
+export default App; 
